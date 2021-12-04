7cd5f219c985bb58f906e3dc8528efbc60b798a0
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2021-12-04 15:43:41 +0530

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,18 @@
+# Assignment - 1
+# Change the variables in the first section, so that each if statement resolves as True.
+
+number=21
+second_number=15
+first_array=[1,2,3]
+second_array=[1,2]
+
+if number > 15: 
+  print("1")
+if first_array: 
+  print("2")
+if len(second_array) == 2: 
+  print("3")
+if len(first_array) + len(second_array) == 5: 
+  print("4")
+if first_array and first_array[0] == 1: 
+  print("5")
